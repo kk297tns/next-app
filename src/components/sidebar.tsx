fb78698e3b7c788d13bbe3e47efd2d83b3337170
kk297tns <chan29711@163.com>
2024-04-22 13:46:05 +0800

    feat: complete note feature

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,44 @@
+import React, {Suspense} from 'react'
+import Link from 'next/link'
+import Image from 'next/image';
+// import { f } from '@/lib/redis';
+import SidebarSearchField from '@/components/SidebarSearchField';
+
+import SidebarNoteList from '@/components/SidebarNoteList';
+import EditButton from '@/components/EditButton';
+import NoteListSkeleton from '@/components/NoteListSkeleton'
+
+
+
+export default async function Sidebar() {
+  return (
+    <>
+      <section className="col sidebar">
+        <Link href={'/'} className="link--unstyled">
+          <section className="sidebar-header">
+            <Image
+              className="logo"
+              src="/logo.svg"
+              width="22"
+              height="20"
+              alt=""
+              role="presentation"
+            />
+            <strong>React Notes</strong>
+          </section>
+        </Link>
+        <section className="sidebar-menu" role="menubar">
+        <SidebarSearchField />
+            <EditButton  noteId={null} >New</EditButton>
+           
+        </section>
+        <nav>
+            <Suspense fallback={<NoteListSkeleton />}>
+            <SidebarNoteList  />
+            </Suspense>
+          
+        </nav>
+      </section>
+    </>
+  )
+}
